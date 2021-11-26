24f2f9ab121e9acffa248474c70ca3246f1b11c7
ShinnnVu <63784536+ShinnnVu@users.noreply.github.com>
2021-11-26 19:25:52 +0700

    Fix addition screen and log out

--- src/screens/addition/addition.tsx
+++ src/screens/addition/addition.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Box, Flex, Image, Text, VStack, View } from "native-base";
-import { Modal } from "react-native";
+import { Modal, TouchableOpacity, ScrollView, Alert } from "react-native";
 import { StyleSheet } from "react-native";
 import color from "../../constants/colors";
 import translate from "../../localize";
@@ -13,6 +13,7 @@ import { Screens } from "../../navigations/model";
 import { useIsFocused } from "@react-navigation/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { apiService } from "../../services";
+import { CommonActions } from "@react-navigation/native";
 const leftIconStyle = { color: color.BLUE_LIGHT, size: 6 };
 const rightIconStyle = { size: 6 };
 const Addition = ({ navigation }: { navigation: any }) => {
@@ -27,15 +28,39 @@ const Addition = ({ navigation }: { navigation: any }) => {
             setUser(JSON.parse(user).username);
         }
     };
+
+    const navigationSignIn = () => {
+        navigation.dispatch(
+            CommonActions.reset({
+                index: 0,
+                routes: [{ name: Screens.LOG_IN }],
+            }),
+        );
+    };
+    const logOut = () => {
+        Alert.alert("Log out", "Are you sure you want to log out?", [
+            {
+                text: "Cancel",
+                onPress: () => {},
+                style: "cancel",
+            },
+            { text: "OK", onPress: () => navigationSignIn() },
+        ]);
+    };
     useEffect(() => {
         checkHost();
     }, [isFocused]);
     return (
         <Flex flex={1} bg={color.WHITE} safeArea>
             <Box mx={"10px"} my={"20px"} alignItems={"center"} flex={1}>
-                <VStack space={3}>
+                <VStack space={3} alignItems={"center"}>
                     <Image source={USER_PHOTO_BIG} alt="Errors" />
-                    <Text fontSize={size.font.title.H3} fontFamily={fonts.PoppinsBold} textAlign={"center"}>
+                    <Text
+                        fontSize={size.font.title.H3}
+                        fontFamily={fonts.PoppinsBold}
+                        textAlign={"center"}
+                        numberOfLines={2}
+                    >
                         {user}
                     </Text>
                 </VStack>
@@ -70,20 +95,62 @@ const Addition = ({ navigation }: { navigation: any }) => {
                                 rightIconStyle={rightIconStyle}
                                 text={translate("addition.logout")}
                                 rightIcon="chevron-right"
-                                press={() => navigation.navigate(Screens.LOG_IN)}
+                                press={logOut}
                             />
                         </VStack>
                     </View>
                 </View>
             </Box>
-            <BottomTab
-                homeActive={false}
-                left={() => {
-                    navigation.navigate(Screens.HOME);
+            {!languageModal && !policyModal && (
+                <BottomTab
+                    homeActive={false}
+                    left={() => {
+                        navigation.navigate(Screens.HOME);
+                    }}
+                    right={() => {}}
+                    checkin={() => {}}
+                />
+            )}
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={languageModal}
+                onRequestClose={() => {
+                    setlanguageModal(false);
                 }}
-                right={() => {}}
-                checkin={() => {}}
-            />
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <TouchableOpacity onPress={() => setlanguageModal(false)}>
+                            <Text fontSize={size.font.text.subTitle} fontFamily={fonts.PoppinsSemiBold} px={"10px"}>
+                                {"English"}
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={policyModal}
+                onRequestClose={() => {
+                    setPolicyModal(false);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <ScrollView contentContainerStyle={styles.modalView}>
+                        <Text fontSize={size.font.text.subTitle} fontFamily={fonts.PoppinsSemiBold}>
+                            {"Policy"}
+                        </Text>
+                        <Text fontSize={size.font.text.small} fontFamily={fonts.PoppinsRegular}>
+                            {translate("policy.text") + translate("policy.text")}
+                        </Text>
+                        <TouchableOpacity onPress={() => setPolicyModal(false)} style={styles.alignSelf}>
+                            <Text>OK</Text>
+                        </TouchableOpacity>
+                    </ScrollView>
+                </View>
+            </Modal>
         </Flex>
     );
 };
@@ -109,7 +176,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        marginTop: 22,
     },
     modalView: {
         margin: 20,
@@ -125,6 +191,9 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.25,
         shadowRadius: 4,
         elevation: 5,
+    },
+    alignSelf: {
+        alignSelf: "flex-end",
     },
 });
 export default Addition;
